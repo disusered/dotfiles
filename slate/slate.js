c9c84360352bc5f08550f9e91ae875b3db191652
Carlos Rosquillas <crosquillas@gmail.com>
2015-01-13 12:36:02 -0600

    Move xdotool move command to own function

--- slate/slate.js
+++ slate/slate.js
@@ -1,15 +1,18 @@
 var hyper   = 'ctrl;shift;alt;cmd';
 var xdotool = '$HOME/.macports/bin/xdotool';
 
+function xmove(x, y) {
+  S.shell("/bin/bash -c 'export DISPLAY=:0;" +
+    xdotool + " getactivewindow windowmove --relative -- " +
+    x + " " + (y - 44) + "'");
+}
+
 function nudge_x11_workaround(win, x, y) {
   x *= 25;
   y *= 25;
 
   if (typeof win == "undefined") {
-    // bug: X11 works differently somehow
-    S.shell("/bin/bash -c 'export DISPLAY=:0;" +
-      xdotool + " getactivewindow windowmove --relative -- " +
-      x + " " + (y - 44) + "'");
+    xmove(x, y);
   } else {
     if (x == 0) x = "+0";
     else if (x > 0) x = "+" + x + "%";
