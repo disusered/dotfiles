d2a49733c302e2acea27f25c0f7af0e36805d0a9
Carlos Rosquillas <crosquillas@gmail.com>
2015-01-14 12:33:38 -0600

    Remove unused code

--- slate/slate.js
+++ slate/slate.js
@@ -49,23 +49,6 @@ function xsize() {
       display.width + " " + display.height + "'");
 }
 
-function nudge_x11_workaround(win, x, y) {
-  x *= 25;
-  y *= 25;
-
-  if (typeof win == "undefined") {
-    xmove(x, y);
-  } else {
-    if (x === 0) x = "+0";
-    else if (x > 0) x = "+" + x + "%";
-    else x = x + "%";
-    if (y == 0) y = "+0";
-    else if (y > 0) y = "+" + y + "%";
-    else y = y + "%";
-    win.doOperation(S.operation("nudge", { "x":x, "y":y }));
-  }
-}
-
 function fullscreen(win) {
   var fs = slate.operation('move', {
     'x' : 'screenOriginX',
@@ -82,8 +65,3 @@ function fullscreen(win) {
 }
 
 S.bind('k' + hyper,  function(win) { fullscreen(win); });
-
-S.bind('left:cmd;alt',  function(win) { nudge_x11_workaround(win, -1, 0); });
-S.bind('right:cmd;alt', function(win) { nudge_x11_workaround(win,  1, 0); });
-S.bind('up:cmd;alt',    function(win) { nudge_x11_workaround(win, 0, -1); });
-S.bind('down:cmd;alt',  function(win) { nudge_x11_workaround(win, 0,  1); });
