2ffa6fe331ef9277d83ad15137b000091fcb8564
Carlos Rosquillas <crosquillas@gmail.com>
2016-12-04 19:46:32 -0800

    Move hyper binds to keybinds module

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -1,16 +1,6 @@
 -- hyper global variable
 k = hs.hotkey.modal.new({}, "F17")
 
--- trigger existing hyper key shortcuts
-hyperUp = function() k.triggered = true end
-hyperDown = function(key)
-  return function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, key) end
-end
-
-k:bind({}, "p", hyperUp, hyperDown("p"));
-k:bind({}, "1", hyperUp, hyperDown("1"));
-k:bind({}, "c", hyperUp, hyperDown("c"));
-
 -- enter hyper-mode when F18 is pressed
 pressedF18 = function()
   k.triggered = false
--- hammerspoon/keybinds.lua
+++ hammerspoon/keybinds.lua
@@ -1,3 +1,13 @@
+-- trigger existing hyper key shortcuts
+hyperUp = function() k.triggered = true end
+hyperDown = function(key)
+  return function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, key) end
+end
+
+k:bind({}, "p", hyperUp, hyperDown("p"));
+k:bind({}, "1", hyperUp, hyperDown("1"));
+k:bind({}, "c", hyperUp, hyperDown("c"));
+
 -- shift+delete pipe |
 hs.hotkey.bind({"shift"}, "delete", nil, function()
   hs.eventtap.keyStroke({"shift"}, "\\")
