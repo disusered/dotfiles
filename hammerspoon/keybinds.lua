6bcf8c6824fbf8fb609d52a0190c03527a47cde9
Carlos Rosquillas <crosquillas@gmail.com>
2016-12-08 22:33:45 -0800

    Add mediakey bindings

--- hammerspoon/keybinds.lua
+++ hammerspoon/keybinds.lua
@@ -12,6 +12,32 @@ k:bind({}, "d", hyperUp, hyperDown("d"));
 k:bind({}, "z", hyperUp, hyperDown("z"));
 k:bind({}, "g", hyperUp, hyperDown("g"));
 
+-- media keys
+mediaKeys = {}
+mediaKeys["f1"]  = "BRIGHTNESS_DOWN"
+mediaKeys["f2"]  = "BRIGHTNESS_UP"
+mediaKeys["f5"]  = "ILLUMINATION_DOWN"
+mediaKeys["f6"]  = "ILLUMINATION_UP"
+mediaKeys["f7"]  = "PREVIOUS"
+mediaKeys["f8"]  = "PLAY"
+mediaKeys["f9"]  = "NEXT"
+mediaKeys["f10"] = "MUTE"
+mediaKeys["f11"] = "SOUND_DOWN"
+mediaKeys["f12"] = "SOUND_UP"
+
+for key, action in pairs(mediaKeys) do
+  local event = require("hs.eventtap").event
+  k:bind({}, key, function()
+    k.triggered = true
+    event.newSystemKeyEvent(action, true):post()
+  end, function()
+    event.newSystemKeyEvent(action, false):post()
+  end, function()
+    event.newSystemKeyEvent(action, true):post()
+    hs.timer.usleep(50000)
+    event.newSystemKeyEvent(action, false):post()
+  end)
+end
 
 -- hyper+delete
 k:bind({}, "delete", hyperUp, function()
