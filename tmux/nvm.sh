ab981948411fa961bebb8a5a083961e20248f5c4
Carlos Rosquillas <crosquillas@gmail.com>
2017-05-30 18:34:25 -0700

    Replace nvm with asdf

--- tmux/nvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PANE_PATH=`tmux display-message -p -F "#{pane_current_path}"`
-if [ -f "$PANE_PATH/.nvmrc" ]
-then
-  echo "#[fg=colour2] #[fg=colour20]`/bin/cat $PANE_PATH/.nvmrc` "
-fi
