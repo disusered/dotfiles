16f4ed1f2168a1095eae864da097753957ae351e
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-06 23:07:19 -0700

    Move vim cmd init

--- neovim-beta/.config/nvim-beta/lua/install_packer.lua
+++ neovim-beta/.config/nvim-beta/lua/install_packer.lua
@@ -7,3 +7,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
 end
+
+-- Only required since we have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
