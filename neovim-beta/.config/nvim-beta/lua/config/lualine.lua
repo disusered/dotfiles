85140d9b5f0c710c3a3a2358291fe1511d283d9a
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-07 03:15:12 -0700

    Tweak lualine

--- neovim-beta/.config/nvim-beta/lua/config/lualine.lua
+++ neovim-beta/.config/nvim-beta/lua/config/lualine.lua
@@ -5,5 +5,8 @@ require('lualine').setup {
   options = {
     -- Enable theme - https://github.com/folke/tokyonight.nvim#-usage
     theme = 'tokyonight'
-  }
+  },
+
+  -- https://github.com/hoob3rt/lualine.nvim#extensions
+  extensions = {'quickfix', 'fugitive'}
 }
--- neovim-beta/.config/nvim-beta/lua/plugins.lua
+++ neovim-beta/.config/nvim-beta/lua/plugins.lua
@@ -65,13 +65,6 @@ return require('packer').startup(function(use)
   -- Post-install/update hook with call of vimscript function with argument
   -- use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
 
-  -- TODO: Enable and configure statusbar
-  -- Use specific branch, dependency and run lua file after load
-  -- use {
-  --   'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
-  --   requires = {'kyazdani42/nvim-web-devicons'}
-  -- }
-
   -- TODO: Enable and configure GitGutter replacements
   -- Use dependency and run lua function after load
   -- use {
