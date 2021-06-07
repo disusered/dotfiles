c10c077ddd6d4644731bd215c196ecfb92e4693c
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-06 23:17:31 -0700

    Integrate TreeSitter and vim-matchup

--- neovim-beta/.config/nvim-beta/lua/config/treesitter.lua
+++ neovim-beta/.config/nvim-beta/lua/config/treesitter.lua
@@ -1,2 +1,22 @@
-local ts = require 'nvim-treesitter.configs'
-ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+-- https://github.com/nvim-treesitter/nvim-treesitter
+
+require 'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = 'maintained',
+
+  -- List of parsers to ignore installing
+  ignore_install = {},
+
+  -- Enable TreeSitter highlight
+  highlight = {
+    enable = true,
+    disable = {}
+  },
+
+  -- Enable TreeSitter integration with vim-matchup
+  -- https://github.com/andymass/vim-matchup
+  matchup = {
+    enable = true, -- mandatory, false will disable the whole extension
+    disable = {},  -- optional, list of language that will be disabled
+  },
+}
