161e3496bddc4c6396e8c077300fbda645de0b9d
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-07 02:20:08 -0700

    Add more telescope commands

--- neovim-beta/.config/nvim-beta/lua/config/telescope.lua
+++ neovim-beta/.config/nvim-beta/lua/config/telescope.lua
@@ -1,4 +1,6 @@
 Map('n', '<leader>p', [[<cmd>lua require('telescope.builtin').git_files()<cr>]], {})
 Map('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], {})
+Map('n', '!', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], {})
+Map('n', '<leader>l', [[<cmd>lua require('telescope.builtin').builtin()<cr>]], {})
 Map('n', '<leader>g', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
 Map('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], {})
