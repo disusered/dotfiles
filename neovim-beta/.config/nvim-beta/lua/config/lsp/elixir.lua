fddfce4d7b3beb34579ef0b48192cdfcc9f58128
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-09 01:07:03 -0700

    Use relative home for ElixirLS

--- neovim-beta/.config/nvim-beta/lua/config/lsp/elixir.lua
+++ neovim-beta/.config/nvim-beta/lua/config/lsp/elixir.lua
@@ -1,4 +1,4 @@
 require'lspconfig'.elixirls.setup{
   -- $HOME and ~ are not expanded
-  cmd = { "/Users/carlos/.dotfiles/vendor/elixir-ls/release/language_server.sh" }
+  cmd = { vim.loop.os_homedir().."/.dotfiles/vendor/elixir-ls/release/language_server.sh" }
 }
