474c5a2a6996ff50ded53cc196bef109eb9a993e
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-09 23:44:33 -0700

    Fix bug with fugitive keybind

--- neovim-beta/.config/nvim-beta/lua/config/fugitive.lua
+++ neovim-beta/.config/nvim-beta/lua/config/fugitive.lua
@@ -10,7 +10,7 @@ wk.register({
     name = "Git",
     s = { ':Git<CR>', 'Git status (interactive)', noremap=true, silent = true },
     w = { ':Gwrite<CR>', 'Git write current file', noremap=true, silent = true },
-    r = { ':Gwrite<CR>', 'Git read current file', noremap=true, silent = true },
+    r = { ':Gread<CR>', 'Git read current file', noremap=true, silent = true },
     b = { ':Gblame<CR>', 'Git blame current file', noremap=true, silent = true },
     g = { ':Git ', 'Git command', noremap=true, silent = false },
   },
