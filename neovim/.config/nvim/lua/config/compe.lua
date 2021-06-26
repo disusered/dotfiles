f50f89f56429b46ec582a80c77fc12ae4e83eda4
Carlos Rosquillas <crosquillas@gmail.com>
2021-06-26 11:07:58 -0700

    Allow confirming completion with Tab

--- neovim/.config/nvim/lua/config/compe.lua
+++ neovim/.config/nvim/lua/config/compe.lua
@@ -23,16 +23,36 @@ require'compe'.setup {
   };
 }
 
--- Allow expansion of completions and snippets with Enter key
-Map("i", "<CR>", "compe#confirm('<CR>')", {noremap = true, silent = true, expr = true});
+-- Complete, expand or jump based on context
+-- https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c
+-- https://github.com/hrsh7th/vim-vsnip#2-setting
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return vim.fn["compe#confirm"]()
+  elseif vim.fn.call("vsnip#available", {1}) == 1 then
+    return t("<Plug>(vsnip-expand-or-jump)")
+  else
+    return t("<Tab>")
+  end
+end
 
 -- Map Tab and S-Tab for expand and/or jump depending on context
--- https://github.com/hrsh7th/vim-vsnip#2-setting
-Map("i", "<Tab>", "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<Tab>'", { noremap = false, expr = true });
-Map("s", "<Tab>", "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<Tab>'", { noremap = false, expr = true });
+Map("i", "<Tab>", "v:lua.tab_complete()", {noremap = false, expr = true})
+Map("s", "<Tab>", "v:lua.tab_complete()", {noremap = false, expr = true})
 
 Map("i", "<S-Tab>", "vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", { noremap = false, expr = true });
 Map("s", "<S-Tab>", "vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", { noremap = false, expr = true });
+
+-- Allow expansion of completions and snippets with Enter key
+Map("i", "<CR>", [[compe#confirm("<CR>")]], {noremap = false, expr = true, silent = true})
+
+-- Allow closing completion PUM with C-e
+Map("i", "<C-e>", [[compe#close("<C-e>")]], {noremap = false, expr = true, silent = true})
 
 -- Enable snippets in additional contexts
 vim.g.vsnip_filetypes = {
