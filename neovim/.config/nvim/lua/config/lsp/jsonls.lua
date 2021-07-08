20e8f6601100a6defe6d7ae8a0c5cf35845886df
Carlos Rosquillas <crosquillas@gmail.com>
2021-07-08 13:34:15 -0700

    Add JSON language server

---
+++ neovim/.config/nvim/lua/config/lsp/jsonls.lua
@@ -0,0 +1,40 @@
+local nvim_lsp = require('lspconfig')
+
+-- Enable snippetSupport in capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.jsonls.setup{
+  -- Attach capabilities to language server
+  capabilities = capabilities,
+
+  on_attach = function(client, bufnr)
+    -- Register keymaps
+    require'config.lsp.maps'(bufnr)
+
+    -- Attach LSP signature plugin and config
+    require'config.lspsignature'()
+
+    -- Attach LSP kind plugin and config
+    require'config.lspkind'()
+  end,
+
+  cmd = { "vscode-json-language-server", "--stdio" },
+
+  filetypes = { "json" },
+
+  init_options = {
+    provideFormatter = true
+  },
+
+  root_dir = nvim_lsp.util.root_pattern(".git", vim.fn.getcwd()),
+
+  commands = {
+    Format = {
+      function()
+        vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
+      end
+    }
+  }
+}
+
