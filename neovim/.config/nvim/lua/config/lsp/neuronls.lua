fd40349cdb62cb3500db98d1668323eeec70d78d
Carlos Rosquillas <crosquillas@gmail.com>
2021-08-09 13:41:27 -0700

    Add neuron ls to editor

---
+++ neovim/.config/nvim/lua/config/lsp/neuronls.lua
@@ -0,0 +1,28 @@
+local nvim_lsp = require'lspconfig'
+local configs = require'lspconfig/configs'
+
+configs.neuronls = {
+  default_config = {
+    cmd = { vim.loop.os_homedir().."/.nix-profile/bin/neuron", "lsp"};
+    filetypes = {'markdown'};
+    root_dir = function()
+      return vim.loop.cwd()
+    end;
+    settings = {};
+  };
+}
+
+nvim_lsp.neuronls.setup{
+  root_dir = nvim_lsp.util.root_pattern("neuron.dhall"),
+
+  on_attach = function(client, bufnr)
+    -- Register keymaps
+    require'config.lsp.maps'(bufnr)
+
+    -- Attach LSP signature plugin and config
+    require'config.lspsignature'()
+
+    -- Attach LSP kind plugin and config
+    require'config.lspkind'()
+  end
+}
