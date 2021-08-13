48262df33018de45b5320da45a0e9822b5c2057d
Carlos Rosquillas <crosquillas@gmail.com>
2021-08-13 12:33:45 -0700

    Add terraform ls

---
+++ neovim/.config/nvim/lua/config/lsp/terraformls.lua
@@ -0,0 +1,20 @@
+-- Enable snippetSupport in capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.terraformls.setup {
+  -- Attach capabilities to CSS language server
+  capabilities = capabilities,
+
+  on_attach = function(client, bufnr)
+    -- Register keymaps
+    require'config.lsp.maps'(bufnr)
+
+    -- Attach LSP signature plugin and config
+    require'config.lspsignature'()
+
+    -- Attach LSP kind plugin and config
+    require'config.lspkind'()
+  end
+}
+
