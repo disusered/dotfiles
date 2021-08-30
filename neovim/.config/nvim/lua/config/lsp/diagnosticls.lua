fb2b39585ea8efa64f6026e7ee2452bd2fe27de3
Carlos Rosquillas <crosquillas@gmail.com>
2021-08-30 12:51:01 -0700

    Add diagnosticls for custom prettier support

---
+++ neovim/.config/nvim/lua/config/lsp/diagnosticls.lua
@@ -0,0 +1,42 @@
+local filetypes = {
+  javascript = "prettier",
+  javascriptreact = "prettier",
+  typescript = "prettierts",
+  typescriptreact = "prettierts",
+}
+
+local formatters = {
+  prettier = {command = "prettier", args = {"--stdin-filepath", "%filepath"}},
+  prettierts = {command = "prettier", args = {"--parser", "typescript", "--stdin-filepath", "%filepath"}}
+}
+
+-- Enable snippetSupport in capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.diagnosticls.setup{
+  -- Attach capabilities to language server
+  capabilities = capabilities,
+
+  on_attach = function(client, bufnr)
+    -- Register keymaps
+    require'config.lsp.maps'(bufnr)
+
+    -- Attach LSP signature plugin and config
+    require'config.lspsignature'()
+
+    -- Attach LSP kind plugin and config
+    require'config.lspkind'()
+  end,
+
+  filetypes = vim.tbl_keys(filetypes),
+
+  init_options = {
+    -- TODO: Add linter
+    -- https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c
+    formatters = formatters,
+    formatFiletypes = filetypes
+  }
+}
+
+
